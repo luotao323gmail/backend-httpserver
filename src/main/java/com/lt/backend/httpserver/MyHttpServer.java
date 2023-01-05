08bb3c0ab16a7fa60b1669acdf0388e5cd1f6e1b
luotao323@gmail.com <luotao323@gmail.com>
2023-01-05 23:41:31 +0900

    commit

--- src/main/java/com/lt/backend/httpserver/MyHttpServer.java
+++ src/main/java/com/lt/backend/httpserver/MyHttpServer.java
@@ -97,6 +97,7 @@ public class MyHttpServer {
         byteBuffer.flip();
         byte[] array = byteBuffer.array();
         String str = new String(array, 0, n);
+        System.out.println("str = " + str);
 //        logger.info("Receive request from client:----------------");
 //        logger.info(str);
     }
