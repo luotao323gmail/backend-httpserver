c696737ec5b1534bf5e64ec9b9fa9fd16214ee7a
luotao323@gmail.com <luotao323@gmail.com>
2023-01-06 22:30:28 +0900

    commit

--- src/main/java/com/lt/backend/httpserver/http/LoginService.java
+++ src/main/java/com/lt/backend/httpserver/http/LoginService.java
@@ -1,8 +1,24 @@
 package com.lt.backend.httpserver.http;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class LoginService implements Service{
     @Override
     public void doPost(Request request, Response response) {
-        response.println("<html><body>Welcome!</body></html>");
+        try {
+            String fileName = "E:\\project\\backend\\backend-httpserver\\src\\main\\webapp\\index.html";
+            // 读取文件内容到Stream流中，按行读取
+            Stream<String> lines = Files.lines(Paths.get(fileName));
+            // 随机行顺序进行数据处理
+            lines.forEach(response::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
--- src/main/java/com/lt/backend/httpserver/http/Request.java
+++ src/main/java/com/lt/backend/httpserver/http/Request.java
@@ -3,6 +3,8 @@ package com.lt.backend.httpserver.http;
 public class Request {
 
     public static Request buildByHttpRequestBytes(byte[] bytes){
+        String str = new String(bytes);
+        System.out.println("str = " + str);
         Request httpRequest = new Request();
 
         return httpRequest;
